83bf82e24a91693ec996a625adcb02296910c123
Nathalia Moniz Cordova <nathaliamonizcordova@gmail.com>
2025-05-24 01:04:56 +0200

    class and function components

---
+++ my-react-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MyButton from './components/MyButton'
+import ContadorClase from './components/ContadorClase'
+
+function App() {
+
+  return (
+    <>
+      <ContadorClase/>
+    </>
+  )
+}
+
+export default App
